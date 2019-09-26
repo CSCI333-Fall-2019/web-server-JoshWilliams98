d2281f8950b60df7e8eae1927533a6241bdd1f34
Josh Williams <josh.williams@principia.edu>
2019-09-25 23:58:59 -0500

    Anton is Alive

---
+++ Anton/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Start";
+            button2.Text = "Stop";
+                
+        }
+
+        private void AntonIntestine(object input)
+        {
+            Thread.Sleep(5000);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            ThreadPool.QueueUserWorkItem(AntonIntestine, this);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+    }
+}
